3af93ad641752d4dcb8179623a7c90b6fcfb73d6
David Nelson <nelsondavidmark@gmail.com>
2022-04-15 12:22:22 -0400

    Add Navbar and Hero.

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'dn-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavbarComponent {}
